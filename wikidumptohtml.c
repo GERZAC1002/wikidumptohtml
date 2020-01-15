9ef0fefbb76272b59291962d8aba9c761830ef0c
GERZAC1002 <gernot.zacharias@gmx.de>
2020-01-15 23:52:51 +0100

    Fixed wrong == that should be =

--- wikidumptohtml.c
+++ wikidumptohtml.c
@@ -130,7 +130,7 @@ int main(int argc,char *argv[]){
 							seite[i] = '_';
 						}
 						if(seite[i] == ' '){
-							seite[i] == '_';
+							seite[i] = '_';
 						}
 					}
 					seite[seitel+0] = '.';
